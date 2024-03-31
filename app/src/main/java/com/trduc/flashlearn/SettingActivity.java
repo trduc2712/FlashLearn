3234007b7c02856662352f38de3a94982f784f3e
Duccuong <quyvuongpc2003@gmail.com>
2024-03-31 23:39:51 +0700

    done fix information user trong menu

--- app/src/main/java/com/trduc/flashlearn/SettingActivity.java
+++ app/src/main/java/com/trduc/flashlearn/SettingActivity.java
@@ -1,5 +1,6 @@
 package com.trduc.flashlearn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -15,6 +16,14 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SettingActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
@@ -22,22 +31,15 @@ public class SettingActivity extends AppCompatActivity {
     LinearLayout home, setting, share, about, logout;
 
     TextView textView;
+    TextView tvEmail, tvUsername;
     Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-
-        drawerLayout = findViewById(R.id.drawerLayout);
-        menu = findViewById(R.id.menu);
-        home = findViewById(R.id.home);
-        about = findViewById(R.id.about);
-        setting = findViewById(R.id.setting);
-        share = findViewById(R.id.share);
-        logout = findViewById(R.id.sign_out);
-
+        initUi();
+        showInformationUser();
 
 
         menu.setOnClickListener(new View.OnClickListener() {
@@ -89,7 +91,17 @@ public class SettingActivity extends AppCompatActivity {
 //            }
 //        });
     }
-
+    private void initUi(){
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        about = findViewById(R.id.about);
+        setting = findViewById(R.id.setting);
+        share = findViewById(R.id.share);
+        logout = findViewById(R.id.sign_out);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvUsername = findViewById(R.id.tvUsername);
+    }
     public static void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer(GravityCompat.START);
     }
@@ -111,5 +123,30 @@ public class SettingActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         closeDrawer(drawerLayout);
+    }
+    private void showInformationUser() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            return;
+        }
+
+        String userID = user.getUid();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Registered users");
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userprofile = snapshot.getValue(User.class);
+                String email = user.getEmail();
+                String name = userprofile.getUsername();
+                tvEmail.setText(email);
+                tvUsername.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/trduc/flashlearn/ShareActivity.java
+++ app/src/main/java/com/trduc/flashlearn/ShareActivity.java
@@ -1,5 +1,6 @@
 package com.trduc.flashlearn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -10,12 +11,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ShareActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ImageView menu;
+    TextView tvEmail, tvUsername;
     LinearLayout home, setting, share, about, sign_out;
 
     @Override
@@ -24,13 +35,9 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_share);
 
 
-        drawerLayout = findViewById(R.id.drawerLayout);
-        menu = findViewById(R.id.menu);
-        home = findViewById(R.id.home);
-        about = findViewById(R.id.about);
-        sign_out = findViewById(R.id.sign_out);
-        setting = findViewById(R.id.setting);
-        share = findViewById(R.id.share);
+
+        initUi();
+        showInformationUser();
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +74,43 @@ public class ShareActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ShareActivity.this, "Đăng xuất", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initUi() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        about = findViewById(R.id.about);
+        sign_out = findViewById(R.id.sign_out);
+        setting = findViewById(R.id.setting);
+        share = findViewById(R.id.share);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvUsername = findViewById(R.id.tvUsername);
+    }
+    private void showInformationUser() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            return;
+        }
+
+        String userID = user.getUid();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Registered users");
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userprofile = snapshot.getValue(User.class);
+                String email = user.getEmail();
+                String name = userprofile.getUsername();
+                tvEmail.setText(email);
+                tvUsername.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
