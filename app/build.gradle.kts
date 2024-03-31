fb58d6f2f8baae2348995e896a7bee2813fd41a9
Huyen <01thanggg3@gmail.com>
2024-03-31 23:15:18 +0700

    xong giao dien phan cai dat activity_setting.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.android.gms:play-services-auth:21.0.0")
 
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("androidx.recyclerview:recyclerview-selection:1.1.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 }
--- app/src/main/java/com/trduc/flashlearn/SettingActivity.java
+++ app/src/main/java/com/trduc/flashlearn/SettingActivity.java
@@ -8,28 +8,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ImageView menu;
-    LinearLayout home, setting, share, about, sign_out;
+    LinearLayout home, setting, share, about, logout;
+
+    TextView textView;
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+
         drawerLayout = findViewById(R.id.drawerLayout);
         menu = findViewById(R.id.menu);
         home = findViewById(R.id.home);
         about = findViewById(R.id.about);
-        sign_out = findViewById(R.id.sign_out);
         setting = findViewById(R.id.setting);
         share = findViewById(R.id.share);
+        logout = findViewById(R.id.sign_out);
+
+
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,12 +71,23 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
-        sign_out.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SettingActivity.this, "Đăng xuất", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(SettingActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(aSwitch.isChecked()) {
+//                    textView.setText("On");
+//                } else {
+//                    textView.setText("Off");
+//                }
+//            }
+//        });
     }
 
     public static void openDrawer(DrawerLayout drawerLayout) {
